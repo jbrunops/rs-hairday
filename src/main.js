e35535a4977a2bc59b4c5bea160e416323190bc0
Jackson Porciúncula <jbrunops@outlook.com>
2025-01-10 16:29:52 -0300

    pacote dayjs instalado e libs criado

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 "use strict";
 
+// Configuração do dayjs
+import "./libs/dayjs";
+
+// CSS
 import "./styles/global.css";
 import "./styles/form.css";
 import "./styles/schedule.css";
+
+// import dayjs from "dayjs";
+// console.log(dayjs()).format("HH:mm");
