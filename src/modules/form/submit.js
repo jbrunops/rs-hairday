d7c86b67d77bd6f07a3fc412750cddbac79bd65c
Jackson Porciúncula <jbrunops@outlook.com>
2025-01-10 17:50:02 -0300

    capturando informações de um formulário e enviando para API de agendamento

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,4 +1,14 @@
+import dayjs from "dayjs";
+
 const form = document.querySelector("form");
+const selectedDate = document.getElementById("date");
+
+// Carrega a data atual
+selectedDate.value = dayjs(new Date()).format("YYYY-MM-DD");
+
+// Definir a data mínima como sendo a data atual
+selectedDate.min = dayjs(new Date()).format("YYYY-MM-DD");
+
 form.onsubmit = (event) => {
   // previnindo o comportamento padrão do formulário
   event.preventDefault();
